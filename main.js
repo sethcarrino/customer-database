c15f66834ee044b039b4d1a6d0188d32e37a9552
sethcarrino <sethcarrino@gmail.com>
2017-07-12 15:10:30 -0400

    added project files

---
+++ main.js
@@ -0,0 +1,23 @@
+let customer = document.querySelector("#customer");
+let results = customers.results;
+let template = ("");
+
+console.log(customer);
+
+for (var i = 0; i < results.length; i++) {
+  template +=  `
+  <div class="container">
+  <img src=${results[i].picture.large}>
+  <h3>${results[i].name.first} ${results[i].name.last}</h3>
+  <p class="email">${results[i].email}</p>
+  <p class="street">${results[i].location.street}</p>
+  <p class="address">${results[i].location.city}. ${results[i].location.state} ${results[i].location.postcode}</p>
+  <p class="phone">${results[i].phone}</p>
+  <p class="ssn">${results[i].id.value}</p>
+  </div>`
+
+  console.log(template);
+
+}
+
+customer.innerHTML = template;
