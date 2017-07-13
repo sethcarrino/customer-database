0fbaf7831986a143504b09ca8fa402934fbdc92e
sethcarrino <sethcarrino@gmail.com>
2017-07-12 21:12:10 -0400

    modified index.html, main.js, and style.css - getting styles to match

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ for (var i = 0; i < results.length; i++) {
   <div class="container">
   <img src=${results[i].picture.large}>
   <h3>${results[i].name.first} ${results[i].name.last}</h3>
+  <hr>
   <p class="email">${results[i].email}</p>
   <p class="street">${results[i].location.street}</p>
   <p class="address">${results[i].location.city}. ${results[i].location.state} ${results[i].location.postcode}</p>
